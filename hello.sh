c25a3ad04f5ce857914742e33c312301cc4c0f8d
Potato <poltoradnevone@gmail.com>
2023-09-18 20:51:33 +0400

    comments and variables

--- hello.sh
+++ hello.sh
@@ -1,2 +1,11 @@
 #! /bin/bash
-echo "hello world"
+# this is a comment
+echo "hello world" # this is also a comment
+
+echo Our shell name is $BASH
+echo Our shell version is $BASH_VERSION
+echo Our home directory is $HOME
+echo Our working directory is $PWD
+
+name=Alexey
+echo My name is $name
