310f963e5ff65f4d8e6ed9ad08b98d0267ebc5f4
Potato <poltoradnevone@gmail.com>
2023-09-19 07:48:27 +0400

    appending text to file

---
+++ appending.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+echo -e "Enter the name of the file: \c"
+read file_name
+
+if [ -f $file_name ]
+then
+    if [ -w $file_name ]
+    then
+        echo "Type some text. To quit press ctrl+d"
+        # > override
+        # >> append
+        cat >> $file_name
+    else
+        echo "The file don't have write permissions"
+    fi
+else
+    echo "$file_name not exists"
+fi
